37a188e48d7901830619a7579656f6278c88c310
tomo2 <tomonag@docomo.ne.jp>
2016-06-22 09:37:10 +0900

    should contain 'secur' からshould contain 'secure'へ修正

--- spec/base/logs_spec.rb
+++ spec/base/logs_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 # syslog設定
 describe file('/etc/logrotate.d/syslog') do
   it { should contain 'messages' }
-  it { should contain 'secur' }
+  it { should contain 'secure' }
 end
